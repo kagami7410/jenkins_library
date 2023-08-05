56140756de41d95537d9be55a619ab0a61a658c9
sujan <sujangurung10@gmail.com>
2023-08-05 12:37:18 +0100

    using inheritFrom

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -4,29 +4,30 @@ def call(body){
     pipeline {
         agent {
             kubernetes{
-                yaml '''
-                apiVersion: v1
-                kind: Pod
-                spec:
-                    containers:
-                    - name: agent-container
-                      image: sujan7410/docker_java_helm:v1.0.0
-                      command:
-                      - cat
-                      tty: true
-                      volumeMounts:
-                      - name: docker-sock-volume
-                        mountPath: /var/run/docker.sock
-                        readOnly: false
-                       
-                    volumes:
-                    - name: docker-sock-volume
-                      hostPath:
-                        path: "/var/run/docker.sock"
-                        
-                  
-                      
-                    '''
+                inheritFrom 'kube-agent'
+//                yaml '''
+//                apiVersion: v1
+//                kind: Pod
+//                spec:
+//                    containers:
+//                    - name: agent-container
+//                      image: sujan7410/docker_java_helm:v1.0.0
+//                      command:
+//                      - cat
+//                      tty: true
+//                      volumeMounts:
+//                      - name: docker-sock-volume
+//                        mountPath: /var/run/docker.sock
+//                        readOnly: false
+//
+//                    volumes:
+//                    - name: docker-sock-volume
+//                      hostPath:
+//                        path: "/var/run/docker.sock"
+//
+//
+//
+//                    '''
             }
         }
 
@@ -46,17 +47,13 @@ def call(body){
             stage('maven package') {
                 steps {
                     script{
-                        container("agent-container")
-                                {
-                                    //                     def mvnHOME = tool name: 'maven', type: 'maven'
-                                    sh 'java -version'
-                                    sh "mvn -version"
-                                    sh 'mvn compile'
-                                    sh "mvn clean package"
-                                    sh 'docker pull openjdk:17'
-//                            sh 'helm template basic-helm-charts/basicHelmChart --valuesbasic-helm-charts/basicHelmChart/values.yaml  '
-                                    sh 'helm create test_template'
-                                }
+                        sh 'java -version'
+                        sh "mvn -version"
+                        sh 'mvn compile'
+                        sh "mvn clean package"
+                        sh 'docker pull openjdk:17'
+                        sh 'helm create test_template'
+
                     }
                 }
             }
