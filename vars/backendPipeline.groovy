43356c1b88f19e95925390c800840c1a940a578b
sujan <sujangurung10@gmail.com>
2023-08-17 16:48:25 +0100

    priniting host name

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -19,41 +19,41 @@ def call(body){
                 steps {
                     script {
                         echo " Testing Environment Variable: ${env.SONAR_QUBE_URL} "
-                        echo "${env.test}"
+                        echo "${env.JENKINS_ENV}"
                         sh 'rm -rf better_backend'
                         sh 'git clone https://github.com/kagami7410/better_backend.git '
                     }
                 }
             }
 
-            stage('maven package') {
-                steps {
-                    sh "mvn clean package"
-                }
-            }
-
-            stage('docker build and push') {
-                steps {
-                    script{
-                        new docker().dockerLogin()
-                        new docker().dockerBuildAndPush("better-backend", "sujan7410")
-                    }
-                }
-            }
-
-
-            stage(' deploy to kubernetes '){
-                steps{
-                    script{
-                        sh """
-                           git clone https://github.com/kagami7410/basic-helm-charts.git
-                           helm template basic-helm-charts/basicHelmChart --values basic-helm-charts/basicHelmChart/values.yaml
-                           helm upgrade helm-test basic-helm-charts/basicHelmChart --values basic-helm-charts/basicHelmChart/values.yaml
-                           """
-
-                    }
-                }
-            }
+//            stage('maven package') {
+//                steps {
+//                    sh "mvn clean package"
+//                }
+//            }
+//
+//            stage('docker build and push') {
+//                steps {
+//                    script{
+//                        new docker().dockerLogin()
+//                        new docker().dockerBuildAndPush("better-backend", "sujan7410")
+//                    }
+//                }
+//            }
+//
+//
+//            stage(' deploy to kubernetes '){
+//                steps{
+//                    script{
+//                        sh """
+//                           git clone https://github.com/kagami7410/basic-helm-charts.git
+//                           helm template basic-helm-charts/basicHelmChart --values basic-helm-charts/basicHelmChart/values.yaml
+//                           helm upgrade helm-test basic-helm-charts/basicHelmChart --values basic-helm-charts/basicHelmChart/values.yaml
+//                           """
+//
+//                    }
+//                }
+//            }
         }
     }
 }
