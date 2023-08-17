b6dfb6ec332a2975b0d3a708dc15d7a38cdb8474
sujan <sujangurung10@gmail.com>
2023-08-17 15:47:29 +0100

    priniting host name

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -18,7 +18,7 @@ def call(body){
             stage('set up') {
                 steps {
                     script {
-                        echo " Testing Environment Variable: ${env.SONAR_QUBE_URL} "
+                        echo " Testing Environment Variable: ${SONAR_QUBE_URL} "
                         sh 'rm -rf better_backend'
                         sh 'git clone https://github.com/kagami7410/better_backend.git '
                     }
