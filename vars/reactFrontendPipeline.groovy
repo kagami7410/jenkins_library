f8394b5d3133fa19a25dfcf30f39bccb0a4f338b
sujan <sujangurung10@gmail.com>
2024-04-06 14:38:16 +0100

    added verisoning print and some helm changes

--- vars/reactFrontendPipeline.groovy
+++ vars/reactFrontendPipeline.groovy
@@ -50,7 +50,7 @@ def call(body){
                            git clone https://github.com/kagami7410/futakai_fe_helm_chart.git
                            cd futakai_fe_helm_chart
                            helm template basicHelmChart
-                           helm upgrade futakai-fe basic-helm-charts -n futakai-fe
+                           helm upgrade futakai-fe basic-helm-charts/ -n futakai-fe
                            """
 
                     }
