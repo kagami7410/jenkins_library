784e540ace3fd769fa6ee7a36d12c3efb92eaab2
sujan <sujangurung10@gmail.com>
2024-04-06 14:10:18 +0100

    added verisoning print and some helm changes

--- vars/reactFrontendPipeline.groovy
+++ vars/reactFrontendPipeline.groovy
@@ -14,33 +14,33 @@ def call(body){
             maven 'maven'
         }
 
-        stages {
-            stage('set up') {
-                steps {
-                    script {
-//                        git remote add origin git@github.com:kagami7410/better_backend.git
-
-                        withCredentials([sshUserPrivateKey(credentialsId: 'github_key', keyFileVariable: 'SSH_KEY')]) {
-
-                            GIT_SSH_COMMAND="ssh -i ${SSH_KEY} git clone git@github.com:kagami7410/site1_pagination.git"
-                            def version = sh(returnStdout: true, script: 'jq -r ".version" package.json').trim()
-                            echo "React App Version: ${version}"
-
-                        }
-
-                    }
-                }
-            }
-
-
-            stage('docker build and push') {
-                steps {
-                    script{
-                        new docker().dockerLogin()
-                        new docker().dockerBuildAndPush("sujan7410", "futakai", "latest")
-                    }
-                }
-            }
+//        stages {
+//            stage('set up') {
+//                steps {
+//                    script {
+////                        git remote add origin git@github.com:kagami7410/better_backend.git
+//
+//                        withCredentials([sshUserPrivateKey(credentialsId: 'github_key', keyFileVariable: 'SSH_KEY')]) {
+//
+//                            GIT_SSH_COMMAND="ssh -i ${SSH_KEY} git clone git@github.com:kagami7410/site1_pagination.git"
+//                            def version = sh(returnStdout: true, script: 'jq -r ".version" package.json').trim()
+//                            echo "React App Version: ${version}"
+//
+//                        }
+//
+//                    }
+//                }
+//            }
+//
+//
+//            stage('docker build and push') {
+//                steps {
+//                    script{
+//                        new docker().dockerLogin()
+//                        new docker().dockerBuildAndPush("sujan7410", "futakai", "latest")
+//                    }
+//                }
+//            }
 
 
             stage(' deploy to kubernetes '){
@@ -48,6 +48,7 @@ def call(body){
                     script{
                         sh """
                            git clone https://github.com/kagami7410/futakai_fe_helm_chart.git
+                           ls
                            helm template /basicHelmChart
                            helm upgrade futakai-fe /basic-helm-charts -n futakai-fe
                            """
