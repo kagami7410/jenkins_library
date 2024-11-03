b9d93d67982e5372511a54dd4b66fc1edcdd91f3
sujan <sujangurung10@gmail.com>
2024-11-03 20:18:32 +0000

    moved exit code logic to publish report

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -105,10 +105,12 @@ def call(body){
                                  keepAll    : true,  // Optional: set to true if you want to keep reports for each build
                                  allowMissing : false  // Set to true if you want to avoid errors if the report is missing
                     ])
-                    if (ZapScanExitCode != 0) {
-                        echo "ZAP baseline scan failed with exit code: ${exitCode}"
-                        currentBuild.result = 'FAILURE' // Mark the build as failed
-                        error("Stopping pipeline due to ZAP failure")
+                    script{
+                        if (ZapScanExitCode != 0) {
+                            echo "ZAP baseline scan failed with exit code: ${exitCode}"
+                            currentBuild.result = 'FAILURE' // Mark the build as failed
+                            error("Stopping pipeline due to ZAP failure")
+                        }
                     }
                 }
             }
