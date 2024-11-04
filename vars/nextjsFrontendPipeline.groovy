983304c6d79e580e30237bb01bf734f34f9c7231
sujan <sujangurung10@gmail.com>
2024-11-04 00:15:52 +0000

    moved exit code logic to publish report

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -106,7 +106,7 @@ def call(body){
             stage('Publish Report') {
                 steps {
                     // Publish the HTML report for viewing in Jenkins
-                    publishHTML([reportDir  : "${REPORT_DIR}",
+                    publishHTML([reportDir  : "/zap/wrk/${REPORT_DIR}",
                                  reportFiles: "${REPORT_FILE}",
                                  reportName : 'OWASP ZAP Report',
                                  alwaysLinkToLastBuild: true,
