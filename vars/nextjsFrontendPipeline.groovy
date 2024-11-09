b38a5078a3237c9d24b7c6eb3e9bbad04a870fb6
sujan <sujangurung10@gmail.com>
2024-11-09 21:40:38 +0000

    added sleep command

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -74,8 +74,6 @@ def call(body){
                             ZapScanExitCode = sh(script:
                                 // Start ZAP in daemon mode and scan the target URL
                                  """
-                                    mkdir -p /zap/wrk/zap_reports 
-                                    chmod +777 /zap/wrk/zap_reports 
                                     python3 /zap/zap-baseline.py \
                                     -t ${TARGET_URL} \
                                     -r /${REPORT_DIR}/${REPORT_FILE} \
