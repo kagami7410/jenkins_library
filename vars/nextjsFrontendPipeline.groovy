1fd88631f60d35fa59ad8399cf6fc471194d25ac
sujan <sujangurung10@gmail.com>
2024-11-09 22:19:12 +0000

    added docker mount

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -47,7 +47,10 @@ def call(body){
                           image: ghcr.io/zaproxy/zaproxy:stable
                           command: [ "cat"]
                           tty: true   
-
+                          volumeMounts:
+                          - name: extra-volume
+                            mountPath: "/zap/wrk/"
+                            readOnly: false
                         - name: node18-container
                           image: node:18
                           command: [ "cat"]
@@ -61,7 +64,9 @@ def call(body){
                         - name: docker-sock-volume
                           hostPath:
                             path: "/var/run/docker.sock"
-
+                        - name: extra-volume
+                          hostPath:
+                            path: "/tmp/"
                     """
             }
         }
