9a3ac897c3c5129b0a153730904ef54f1e7567cd
sujan <sujangurung10@gmail.com>
2024-11-09 23:29:47 +0000

    added docker mount

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -124,6 +124,8 @@ def call(body){
                                  """
                                     mkdir -p /zap/wrk/zap_reports
                                     chmod +777 /zap/wrk/zap_reports
+                                    mkdir -p wrk/zap_reports
+                                    chmod +777 wrk/zap_reports
                                     python3 /zap/zap-baseline.py \
                                     -t ${TARGET_URL} \
                                     -r /${REPORT_DIR}/${REPORT_FILE} \
@@ -144,6 +146,10 @@ def call(body){
 
                             sh """
                                cd /zap/wrk/${REPORT_DIR}
+                               echo "this is testfile!" > testfile.txt
+                               pwd
+
+                               cd wrk/${REPORT_DIR}
                                echo "this is testfile!" > testfile.txt
                                pwd
                                ls -ltr
