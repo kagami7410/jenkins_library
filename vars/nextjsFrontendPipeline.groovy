ff53bd8564cefc04b279974b109d8d7af758da57
sujan <sujangurung10@gmail.com>
2024-11-09 21:48:18 +0000

    added sleep command

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -108,6 +108,10 @@ def call(body){
             stage('Publish Report') {
                 steps {
 
+                    sh """
+
+                        sleep 200
+                       """
                     // Publish the HTML report for viewing in Jenkins
                     publishHTML([reportDir  : "/zap/wrk/${REPORT_DIR}",
                                  reportFiles: "${REPORT_FILE}",
