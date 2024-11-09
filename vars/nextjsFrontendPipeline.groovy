1cebbbf4e00def167cf69387c7c57fc479ae111e
sujan <sujangurung10@gmail.com>
2024-11-09 18:22:06 +0000

    added sleep command

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -78,8 +78,8 @@ def call(body){
                                     chmod +777 wrk/zap_reports 
                                     python3 /zap/zap-baseline.py \
                                     -t ${TARGET_URL} \
-                                    -r /home/jenkins/agent/workspace/squid-corals-frontend/zap/wrk/${REPORT_DIR}/${REPORT_FILE} \
-                                    -J /home/jenkins/agent/workspace/squid-corals-frontend/zap/wrk/${REPORT_DIR}/zap_report.json 
+                                    -r /home/jenkins/agent/workspace/squid-corals-frontend/${REPORT_DIR}/${REPORT_FILE} \
+                                    -J /home/jenkins/agent/workspace/squid-corals-frontend/${REPORT_DIR}/zap_report.json 
                                     """,
                                     returnStatus: true)
 
