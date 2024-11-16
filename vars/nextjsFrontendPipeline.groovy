2e2454e3e3ad33f0a73740c3a4a66f987ce7968c
sujan <sujangurung10@gmail.com>
2024-11-16 20:14:26 +0000

    testing dummy html file

--- vars/nextjsFrontendPipeline.groovy
+++ vars/nextjsFrontendPipeline.groovy
@@ -43,7 +43,7 @@ def call(body){
                             readOnly: false
                           volumeMounts:
                           - name: extra-volume
-                            mountPath: /home/jenkins/agent/workspace/squid-corals-frontend/zap
+                            mountPath: /home/jenkins/agent/workspace/squid-corals-frontend
                             readOnly: false
                         - name: zap
                           image: ghcr.io/zaproxy/zaproxy:stable
