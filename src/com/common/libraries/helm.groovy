4040f81d5f267a02ac5f25f2b0b1626832cbd6e7
sujan <sujangurung10@gmail.com>
2025-01-25 23:35:00 +0000

    added certs -manager in helm library

--- src/com/common/libraries/helm.groovy
+++ src/com/common/libraries/helm.groovy
@@ -4,5 +4,6 @@ def deploy(appName){
     sh """
         ls
         helm upgrade --install ${appName} ./helm_charts/reef-forge-helmchart -n ${appName} --create-namespace
+        kubectl apply -f certs-manager
     """
 }
