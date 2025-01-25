01fb250b4d7cffac075ff4229505bc567431c928
sujan <sujangurung10@gmail.com>
2025-01-25 21:15:24 +0000

    uncommented stages

--- src/com/common/libraries/helm.groovy
+++ src/com/common/libraries/helm.groovy
@@ -3,6 +3,6 @@ package com.common.libraries
 def deploy(appName){
     sh """
         ls
-        helm upgrade --install ${appName} helm_charts/reef-forge-helmchart -n ${appName} --create-namespace
+        helm upgrade --install ${appName} ./helm_charts/reef-forge-helmchart -n ${appName} --create-namespace
     """
 }
