f6e4acbf4a17d251b711193a9b40d0db6753f17b
sujan <sujangurung10@gmail.com>
2025-01-25 20:53:56 +0000

    uncommented stages

--- src/com/common/libraries/helm.groovy
+++ src/com/common/libraries/helm.groovy
@@ -2,6 +2,7 @@ package com.common.libraries
 
 def deploy(appName){
     sh """
-        helm upgrade --install ${appName} ./helm_charts -n ${appName} --create-namespace
+        ls
+        helm upgrade --install ${appName} helm_charts -n ${appName} --create-namespace
     """
 }
